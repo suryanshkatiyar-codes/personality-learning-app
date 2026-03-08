5bf786f2f2626a4408eb9bb635594d52ea39a306
Suryansh Katiyar <2k23.aids2310309@gmail.com>
2026-03-08 22:17:19 +0530

    Day 10: Roadmap display page

--- client/src/pages/Roadmap.jsx
+++ client/src/pages/Roadmap.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import API from '../api/axios';
+import { useAuth } from '../context/AuthContext';
+
 
 const Roadmap = () => {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const[roadmap,setRoadmap]=useState([]);
+  const[loading,setLoading]=useState(true);
+
+  useEffect(() => {
+    const fetchRoadmap = async () => {
+      try {
+        const response = await API.get(`/api/roadmap/${id}`);
+        setRoadmap(response.data.roadmap);
+      } catch (err) {
+        console.log("Failed to fetch that particular roadmap",err);
+      }
+      setLoading(false);
+    }
+    fetchRoadmap();
+  },[])
+
+  if(loading){
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
-      Roadmap page
+      <div>
+        <button onClick={()=>{
+          navigate('/dashboard');
+        }}>Back</button>
+        <div>
+          <div>{roadmap.skill}</div>
+          <div>{roadmap.personalityType}</div>
+        </div>
+      </div>
+      <div>
+        {roadmap.roadmap}
+      </div>
     </div>
   )
 }
